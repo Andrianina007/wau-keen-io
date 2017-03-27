03a11bc773ab84e7335ba27836515891924ef579
Toky Mahefa RALALA <toky@wau.travel>
2017-03-27 11:44:54 +0300

    move podioobservable from WAU-podio to wau-keen-io

---
+++ src/Observer/AbstractPodioObservable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: bundinho
+ * Date: 3/24/17
+ * Time: 2:13 PM
+ */
+
+namespace WauKeenIo\Observer;
+
+
+abstract class AbstractPodioObservable implements ObservableInterface
+{
+  protected $observers =  [];
+
+
+  public abstract function notify();
+
+
+  public function attach(ObserverInterface $observer_in)
+  {
+      $this->observers[] = $observer_in;
+  }
+
+  public function detach(ObserverInterface $observer_in)
+  {
+    foreach($this->observers as $key => $obs) {
+      if($obs == $observer_in) {
+        unset($this->observers[$key]);
+      }
+    }
+  }
+
+  public abstract function getRawData();
+}
